7ee2e19ba062c2a070ffe48b9bcce10916292bd7
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2016-08-08 12:16:44 +0200

    some cleanup

--- setup.py
+++ setup.py
@@ -6,11 +6,19 @@ setup(
     version='1.0',
     description="Tripal API library",
     author="Anthony Bretaudeau",
-    author_email="anthnoy.bretaudeau@inra.fr",
+    author_email="anthony.bretaudeau@inra.fr",
+    url="https://github.com/abretaud/python-tripal"
     install_requires=['requests>=2.4.3'],
     packages=find_packages(),
     license='MIT',
     platforms="Posix; MacOS X; Windows",
+    scripts=[
+        'scripts/get_all_jobs.py',
+        'scripts/get_job.py',
+        'scripts/load_blast.py',
+        'scripts/load_fasta.py',
+        'scripts/load_gff3.py'
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
