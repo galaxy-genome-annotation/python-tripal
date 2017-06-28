f8ef318e820ee83aca6aad113cb14db6cdb18697
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2017-06-28 09:40:49 +0200

    1.9 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import listdir
 
 setup(
     name="tripal",
-    version='1.8',
+    version='1.9',
     description="Tripal API library",
     author="Anthony Bretaudeau",
     author_email="anthony.bretaudeau@inra.fr",
