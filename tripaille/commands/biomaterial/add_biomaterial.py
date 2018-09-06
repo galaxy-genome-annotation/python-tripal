0a2188da18098fd4e4d4634a5c1aae6a15ccf689
mboudet <mateo.boudet@irisa.fr>
2018-09-06 13:18:15 +0000

    Fixes

--- tripaille/commands/biomaterial/add_biomaterial.py
+++ tripaille/commands/biomaterial/add_biomaterial.py
@@ -4,17 +4,19 @@ from tripaille.decorators import custom_exception, dict_output
 
 
 @click.command('add_biomaterial')
-@click.argument("organism_id", type=str, help="Id of the associated organism")
-@click.argument("file_path", type=str, help="Path to the biomaterial file")
-@click.argument("file_type", type=click.Choice(['xml', 'tsv', 'csv']), help="Format of the biomaterial file : xml, tsv or csv")
+@click.argument("organism_id", type=str)
+@click.argument("file_path", type=str)
+@click.argument("file_type", type=click.Choice(['xml', 'tsv', 'csv']))
 @pass_context
 @custom_exception
 @dict_output
 def cli(ctx, organism_id, file_path, file_type):
-    """Add a new biomaterial to the database
-
+    """Add a new biomaterial to the database\n
+   organism_id : Id of the associated organism\n
+   file_path : Path to the biomaterial file\n
+   file_type : Format of the biomaterial file : xml, tsv or csv\n
 Output:
 
-    Biomatarial information
+    Biomaterial information
     """
     return ctx.gi.biomaterial.add_biomaterial(organism_id, file_path, file_type)
--- tripaille/commands/cmd_biomaterial.py
+++ tripaille/commands/cmd_biomaterial.py
@@ -4,7 +4,7 @@ from tripaille.commands.biomaterial.add_biomaterial import cli as func0
 
 @click.group()
 def cli():
-    """Manage Tripal organisms"""
+    """Manage Tripal Biomaterials"""
     pass
 
 
